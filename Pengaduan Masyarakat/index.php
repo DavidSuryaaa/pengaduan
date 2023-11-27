158c001419ec29d29a409a7243486c4bb2e6633a
DavidSuryaaa <152183496+DavidSuryaaa@users.noreply.github.com>
2023-11-27 20:30:52 +0700

    pengaduan masyarakat

---
+++ Pengaduan Masyarakat/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pengaduan Masyarakat</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <div class="nav-toggle">
+                <img src="view/assets/img/Logo.svg" alt="" />
+            </div>
+            <div class="nav-brand">
+                <img src="view/assets/img/teks.svg" alt="">
+            </div>
+            <div class="nav-links">
+                <a href="loginpage.php" class="link-sm">Masuk<img src="view/assets/img/icon.svg" alt=""></a>
+                <a href="register.php">
+                <button class="btn-primary">Daftar</button>
+                </a>
+            </div>
+        </nav>
+<!--Hero-->
+<section class="hero">
+    <div class="column-text-heading">
+        <img class="H1" src="view/assets/img/h1.svg" alt="">
+        <p class="text-p">
+            anda bisa melaporkan semua keluhan anda
+di forum ini
+        </p>
+        <a href="loginpage.php">
+        <button class="btn-secondary">Mulai</button>
+</a>
+<br>
+<br>
+<a href="https://api.whatsapp.com/send?phone=6285697637135" class="link-sm"><img class="wa" src="view/assets/img/chatsmall.png" alt=""></a>
+
+    </div>
+    <div class="column-illustration-hero">
+        <img class="il-hero" src="view/assets/img/vec.svg" alt="">
+
+    </div>
+</section>
+    </div>
+    
+</body>
+</html>
+
